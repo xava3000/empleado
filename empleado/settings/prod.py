58ac8a9ec9cb1e7c8c8a58c16b918711f62c7f61
Xavier Valencia <xavier.valencia@ohdgtl.com>
2021-06-19 20:13:11 +0200

    hosts

--- empleado/settings/prod.py
+++ empleado/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['188.166.16.231', 'localhost', '127.0.0.1']
 
 DATABASES = {
     'default': {
